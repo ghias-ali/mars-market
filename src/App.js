e8955c29c57e24ccc9d57f3b70f1342c82db7752
ah-hadi <hadirajpoot63@gmail.com>
2021-09-05 22:53:41 -0700

    ga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
-
+import Navbar from "./components/navbar.js/index"
+import Products from './components/Productpage/Products';
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>
+
+  <Navbar/>
+  <Products/>
+  </div>;
 }
 
 export default App;
